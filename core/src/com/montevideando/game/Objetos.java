f6e1b9f0654e0d587d1f72a2ed66b71d067dc13e
N0izX1 <n0izx11@gmail.com>
2016-11-08 19:00:51 -0300

    Se agrega clase Objetos

---
+++ core/src/com/montevideando/game/Objetos.java
@@ -0,0 +1,41 @@
+package com.montevideando.game;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.physics.box2d.World;
+import com.uwsoft.editor.renderer.components.DimensionsComponent;
+import com.uwsoft.editor.renderer.components.TransformComponent;
+import com.uwsoft.editor.renderer.scripts.IScript;
+import com.uwsoft.editor.renderer.utils.ComponentRetriever;
+
+public class Objetos implements IScript {
+
+	private Entity object;
+	private TransformComponent transformComponent;
+	private DimensionsComponent dimensionsComponent;
+	private World world;
+	
+	public Objetos(World world){
+		this.world = world;
+	}
+	
+	@Override
+	public void init(Entity entity) {
+		object = entity;
+		transformComponent = ComponentRetriever.get(entity, TransformComponent.class);
+		dimensionsComponent = ComponentRetriever.get(entity, DimensionsComponent.class);
+		
+	}
+
+	@Override
+	public void act(float delta) {
+		// Reaccion del objeto
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+		
+	}
+
+}
